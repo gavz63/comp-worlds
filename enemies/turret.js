2cfe82b24792f397ea6bf104805e2d29be7469b4
Joel Johnson <jjohn96@uw.edu>
2020-02-04 15:41:39 -0800

    Added a turret Pattern

--- enemies/turret.js
+++ enemies/turret.js
@@ -4,7 +4,7 @@ class Turret extends Entity {
 		super(game, x, y);
 		this.rotation = 0;
 		this.timer = null;
-		this.fireRate = 50;
+		this.fireRate = 6;
 		this.lastShotTime = 0;
 		
 		this.projectileSprite = game.AM.getAsset("./img/projectiles/Fireball.png");
@@ -14,14 +14,18 @@ class Turret extends Entity {
         {x: 0, y: 0}, {x: 3, y: 0},
         6, true, STANDARD_DRAW_SCALE);
 		
-		this.attack = this.spiralShot;
+		this.attack = this.crossShot;
+		
+		let that = this;
+		
+		this.attackTimer = new TimerCallback(this.game, 1/this.fireRate, true, function() { that.attack(); });
 		
 		this.game.addEntity(this, "hud");
 	}
 	
 	update()
 	{
-		this.attack();
+		//this.attack();
 	}
 	
 	draw()
@@ -44,5 +48,42 @@ class Turret extends Entity {
 			};
 			new Projectile(this.game, this.x, this.y, dir, 100, 2, this, this.projectileAnimation, 1, 5);
 		}
+	}
+	
+	crossShot()
+	{
+		if(this.timer === null)
+		{
+			this.timer = new Timer(this.game, 3, true);
+		}
+		this.lastShotTime = this.timer.getPercent();
+		if(this.timer.getPercent() > 0.5)
+		{
+			this.rotation = Math.PI/4;
+		}
+		else
+		{
+			this.rotation = 0;
+		}
+		let dir1 = {
+			x: Math.cos(this.rotation + Math.PI * 1/4),
+			y: Math.sin(this.rotation + Math.PI * 1/4)
+		};
+		let dir2 = {
+			x: Math.cos(this.rotation + Math.PI * 3/4),
+			y: Math.sin(this.rotation + Math.PI * 3/4)
+		};
+		let dir3 = {
+			x: Math.cos(this.rotation + Math.PI * 5/4),
+			y: Math.sin(this.rotation + Math.PI * 5/4)
+		};
+		let dir4 = {
+			x: Math.cos(this.rotation + Math.PI * 7/4),
+			y: Math.sin(this.rotation + Math.PI * 7/4)
+		};
+		new Projectile(this.game, this.x, this.y, dir1, 100, 2, this, this.projectileAnimation, 1, 5);
+		new Projectile(this.game, this.x, this.y, dir2, 100, 2, this, this.projectileAnimation, 1, 5);
+		new Projectile(this.game, this.x, this.y, dir3, 100, 2, this, this.projectileAnimation, 1, 5);
+		new Projectile(this.game, this.x, this.y, dir4, 100, 2, this, this.projectileAnimation, 1, 5);
 	}
 }
