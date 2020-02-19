ec2c8bad8694561bb2b62c8e58ee8cd5b8332f69
zeekers63 <gavz63@uw.edu>
2020-02-18 23:26:53 -0800

    progress on controls menu page

--- game/gameengine.js
+++ game/gameengine.js
@@ -6,7 +6,8 @@ const DIRECTION_DOWN = "DOWN";
 const GAME_STATES = {
     CHARACTER_SELECT: "char_select",
     PLAYING: "playing_level",
-    CHANGING_LEVEL: "changing level"
+    CHANGING_LEVEL: "changing level",
+    CONTROLS_TUTORIAL: "controls tutorial"
 };
 
 const LAYERS = {
@@ -51,6 +52,7 @@ class GameEngine {
         this._entities[LAYERS.PLAYER_PROJECTILES] = [];
         this._entities[LAYERS.MAIN] = [];
         this._entities[LAYERS.WALL] = [];
+        this._entities[LAYERS.PARTICLES] = [];
         this._entities[LAYERS.HUD] = [];
         this._ctx = null;
 
@@ -109,12 +111,26 @@ class GameEngine {
      */
     init(ctx) {
         this.audioManager = new AudioManager();
-        this.LoadLevel(new Level1(), [new BlackMage(), new Lancer(), new Ninja()]);
+        this.LoadLevel(new Level1(), [new BlackMage(""), new Lancer(""), new Ninja("")]);
         this._ctx = ctx;
         this._surfaceWidth = this._ctx.canvas.width;
         this._surfaceHeight = this._ctx.canvas.height;
         this.startInput();
         this._clock = new Clock();
+    }
+
+    controlsPageInit(ctx) {
+        this.destroyLevel();
+        this._sceneManager.LoadLevel(new LevelFake());
+
+        this._ctx = ctx;
+        this._surfaceWidth = this._ctx.canvas.width;
+        this._surfaceHeight = this._ctx.canvas.height;
+        this._clock = new Clock();
+        this.game_state = GAME_STATES.CONTROLS_TUTORIAL;
+
+        this.addEntity(this._camera, LAYERS.HUD);
+        this._camera.update();
     }
 
     /**
@@ -262,7 +278,7 @@ class GameEngine {
                 }
 
                 break;
-
+            case GAME_STATES.CONTROLS_TUTORIAL:
             case GAME_STATES.PLAYING:
             case GAME_STATES.CHANGING_LEVEL:
                 for (var i = 0; i < this._entities.length; i++) {
