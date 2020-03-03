f563e549c24e6ef1d7da71b2c6db9de76bf8d8db
zeekers63 <gavz63@uw.edu>
2020-03-03 15:43:51 -0800

    in progress commit for dragon animation and sizing

--- game/enemies/wood_dragon.js
+++ game/enemies/wood_dragon.js
@@ -2,28 +2,109 @@ class WoodDragon extends Enemy {
     constructor(game, x, y, spawner) {
         super(game, x, y, spawner);
 
-        game.addEntity()
+        this.myAddScale = 3;
+        this.myBodyAddScale = 2;
+        this.myScale = [STANDARD_DRAW_SCALE * this.myAddScale];
+        this.myBodyScale = [STANDARD_DRAW_SCALE * this.myBodyAddScale];
+
+        this.flyingLeftAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/DragonFlyingLeftFace.png"),
+            STANDARD_ENTITY_FRAME_WIDTH * 4, STANDARD_ENTITY_FRAME_WIDTH * 4,
+            {x: 0, y: 0}, {x: 5, y: 0},
+            7, true, this.myScale);
+
+        this.flyingRightAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/DragonFlyingRightFace.png"),
+            STANDARD_ENTITY_FRAME_WIDTH * 4, STANDARD_ENTITY_FRAME_WIDTH * 4,
+            {x: 0, y: 0}, {x: 5, y: 0},
+            7, true, this.myScale);
+
+        this.shadowLeftAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/DragonShadowLeftFace.png"),
+            STANDARD_ENTITY_FRAME_WIDTH * 4, STANDARD_ENTITY_FRAME_WIDTH * 4,
+            {x: 0, y: 0}, {x: 5, y: 0},
+            7, true, this.myScale);
+
+        this.shadowRightAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/DragonShadowRightFace.png"),
+            STANDARD_ENTITY_FRAME_WIDTH * 4, STANDARD_ENTITY_FRAME_WIDTH * 4,
+            {x: 0, y: 0}, {x: 5, y: 0},
+            7, true, this.myScale);
+
+        this.takingOffLeftAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/DragonTakingOffLeftFace.png"),
+            STANDARD_ENTITY_FRAME_WIDTH * 4, STANDARD_ENTITY_FRAME_WIDTH * 4,
+            {x: 0, y: 0}, {x: 7, y: 0},
+            7, false, this.myScale);
+
+        this.takingOffRightAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/DragonTakingOffRightFace.png"),
+            STANDARD_ENTITY_FRAME_WIDTH * 4, STANDARD_ENTITY_FRAME_WIDTH * 4,
+            {x: 0, y: 0}, {x: 7, y: 0},
+            7, false, this.myScale);
+
+        this.bodyLeftAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/WoodDragonBodyLeftFace.png"),
+            STANDARD_ENTITY_FRAME_WIDTH * 6, STANDARD_ENTITY_FRAME_WIDTH * 4,
+            {x: 0, y: 0}, {x: 3, y: 0},
+            4, false, this.myBodyScale);
+
+        this.bodyRightAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/WoodDragonBodyRightFace.png"),
+            STANDARD_ENTITY_FRAME_WIDTH * 6, STANDARD_ENTITY_FRAME_WIDTH * 4,
+            {x: 0, y: 0}, {x: 3, y: 0},
+            4, false, this.myBodyScale);
+
+        this.animation = this.bodyRightAnimation;
+        this.animation.pause();
+        this.head = new WoodDragonHead(this.game, this.x + STANDARD_ENTITY_FRAME_WIDTH * 3 , this.y, this.spawner, this);
+    }
+
+    update() {
+        super.update();
+        this.myScale[0] = STANDARD_DRAW_SCALE * this.myAddScale;
+        this.myBodyScale[0] = STANDARD_DRAW_SCALE * this.myBodyAddScale;
     }
 }
 
 class WoodDragonHead extends Enemy {
-    constructor(game, x, y, spawner) {
+    constructor(game, x, y, spawner, dragon) {
         super(game, x, y, spawner);
-        this.breathAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/WoodDragonHeadBreathWeapon"),
+
+        this.myScale = dragon.myScale;
+        this.myAddScale = dragon.myAddScale;
+
+
+        this.stdAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/WoodDragonHead.png"),
+            STANDARD_ENTITY_FRAME_WIDTH, STANDARD_ENTITY_FRAME_WIDTH * 2,
+            {x:0, y: 0}, {x: 0, y: 0},
+            4, false, this.myScale);
+
+        this.breathAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/WoodDragonHeadBreathWeapon.png"),
             STANDARD_ENTITY_FRAME_WIDTH, STANDARD_ENTITY_FRAME_WIDTH * 2,
             {x:0, y: 0}, {x: 1, y: 0},
             4, false, this.myScale);
 
-        this.breathAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/WoodDragonHeadBreathWeapon"),
+        this.biteAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/WoodDragonHeadBite.png"),
             STANDARD_ENTITY_FRAME_WIDTH, STANDARD_ENTITY_FRAME_WIDTH * 2,
             {x:0, y: 0}, {x: 1, y: 0},
             4, false, this.myScale);
+
+        this.postLaunchAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/WoodDragonHeadPostLaunch.png"),
+            STANDARD_ENTITY_FRAME_WIDTH, STANDARD_ENTITY_FRAME_WIDTH * 2,
+            {x:0, y: 0}, {x: 1, y: 0},
+            4, false, this.myScale);
+
+        this.dmgAnimation = new Animation(ASSET_MANAGER.getAsset("./img/enemies/WoodDragon/WoodDragonHeadStunned.png"),
+            STANDARD_ENTITY_FRAME_WIDTH * 2, STANDARD_ENTITY_FRAME_WIDTH * 2,
+            {x:0, y: 0}, {x: 3, y: 0},
+            4, false, this.myScale);
+
+        this.animation = this.stdAnimation;
+        this.animation.pause();
+
+        this.dragon = dragon;
+
+        game.addEntity(this, LAYERS.ENEMIES);
     }
 
     update() {
         super.update();
 
-        this.myScale[0] = STANDARD_DRAW_SCALE;
+        this.myScale[0] = STANDARD_DRAW_SCALE * this.myAddScale;
+
 
 
     }
