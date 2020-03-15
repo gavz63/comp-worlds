68352f26d50c0c212dccdf386062115ab06f7385
zeekers63 <gavz63@uw.edu>
2020-03-14 23:22:42 -0700

    removing posts from spacebar

--- game/input.js
+++ game/input.js
@@ -72,9 +72,9 @@ function Input() {
 
             if (e.code === "Space") {
                 that.spacebar = true;
-                for (let i = 0; i < 32; i++) {
-                    new Post(that, 96 * (Math.ceil(RandomBetween(3, 13)) - 0.5), 96 * (Math.ceil(RandomBetween(0, 8)) - 0.5));
-                }
+                // for (let i = 0; i < 32; i++) {
+                //     new Post(that, 96 * (Math.ceil(RandomBetween(3, 13)) - 0.5), 96 * (Math.ceil(RandomBetween(0, 8)) - 0.5));
+                // }
             }
 
             // Temporary
