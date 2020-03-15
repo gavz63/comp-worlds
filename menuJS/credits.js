ae8f4f8f96af7daab5eb8128cd0177f5ae72ef28
zeekers63 <gavz63@uw.edu>
2020-03-15 16:29:55 -0700

    fixed credits volume

--- menuJS/credits.js
+++ menuJS/credits.js
@@ -1,5 +1,8 @@
 window.onload = () => {
     let audio_manager = new AudioManager();
+    audio_manager.setMasterVolume(parseFloat(sessionStorage.getItem('master_volume')));
+    audio_manager.setMusicVolume(parseFloat(sessionStorage.getItem('music_volume')));
+    audio_manager.setSFXVolume(parseFloat(sessionStorage.getItem('gameplay_volume')));
     audio_manager.setMusic('endMusic');
     audio_manager.playMusic();
 
